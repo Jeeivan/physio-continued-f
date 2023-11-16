ca9a4cba545fd5a52d17b2580f1e477ea20652b8
jeeivan <jeeivansri@gmail.com>
2023-11-16 19:41:48 +0000

    First commit and set up react app and tested a fetch to the back end

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
 
 function App() {
+  const [physioFormData, setPhysioFormData] = useState([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const response = await fetch(`http://localhost:8000/physioform/`,
+          {
+            method: "GET",
+            headers: {
+              "Content-Type": "application/json",
+              'Authorization': 'Basic '+btoa(`${process.env.REACT_APP_USER}:${process.env.REACT_APP_PASSWORD}`),
+            },
+          }
+        );
+        const data = await response.json();
+        setPhysioFormData(data);
+      } catch (error) {
+        console.log("Error fetching physioform data", error);
+      }
+    }
+
+    fetchData();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Physioform List</h1>
+      <ul>
+        {physioFormData.map((physioForm) => (
+          <li key={physioForm.id}>
+            <strong>Date:</strong> {physioForm.date}<br />
+            <strong>Body Part:</strong> {physioForm.body_part}<br />
+            <strong>Time:</strong> {physioForm.time}<br />
+            <strong>Trauma:</strong> {physioForm.trauma}<br />
+            <strong>Location:</strong> {physioForm.location}<br />
+            <strong>Scans:</strong> {physioForm.scans}<br />
+            <strong>Aggravating Factors:</strong> {physioForm.aggs}<br />
+            <strong>Easing Factors:</strong> {physioForm.eases}<br />
+            <strong>Past Treatment:</strong> {physioForm.past_treatment}<br />
+            <strong>Medication:</strong> {physioForm.medication}<br />
+            <strong>Work:</strong> {physioForm.work}<br />
+            <strong>Goals:</strong> {physioForm.goals}<br />
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
